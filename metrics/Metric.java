45631465fd94e58f5c16df7c3f045b7d79708745
oronmark <oron102@gmail.com>
2016-12-29 22:41:02 +0200

    backup

--- metrics/Metric.java
+++ metrics/Metric.java
@@ -1,9 +1,9 @@
 
-public abstract class Metric<P1,P2> {
+public class Metric<P1,P2> {
 //P1- type of distance
 //P2- type if points
 	
-	public abstract P2 calcDistance(P1 p1, P1 p2);
+	public P2 calcDistance(P1 p1, P1 p2){return null;};
 
 
 }
--- metrics/euclidian.java
+++ metrics/euclidian.java
@@ -16,3 +16,5 @@ public class euclidian extends Metric<double[], Double> {
 
 
 }
+
+
